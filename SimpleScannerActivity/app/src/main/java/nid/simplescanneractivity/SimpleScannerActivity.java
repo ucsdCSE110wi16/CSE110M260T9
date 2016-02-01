aeb182ae07c2a251970ccffc8d31d6920e9e2671
Nideesh Terapalli <Nid@nideeshs-mbp.dynamic.ucsd.edu>
2016-01-31 18:10:43 -0800

    QR Template I guess

---
+++ SimpleScannerActivity/app/src/main/java/nid/simplescanneractivity/SimpleScannerActivity.java
@@ -0,0 +1,56 @@
+package nid.simplescanneractivity;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+import com.google.zxing.Result;
+import android.util.Log;
+
+
+public class SimpleScannerActivity extends AppCompatActivity implements ZXingScannerView.ResultHandler{
+
+    private ZXingScannerView mScannerView;
+    private String TAG = "SimeScannerActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mScannerView = new ZXingScannerView(this);   // Programmatically initialize the scanner view
+        setContentView(mScannerView);                // Set the scanner view as the content view
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mScannerView.setResultHandler(this); // Register ourselves as a handler for scan results.
+        mScannerView.startCamera();          // Start camera on resume
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mScannerView.stopCamera();           // Stop camera on pause
+    }
+
+
+    @Override
+    public void handleResult(Result rawResult) {
+        // Do something with the result here
+        Log.v(TAG, rawResult.getText()); // Prints scan results
+        Log.v(TAG, rawResult.getBarcodeFormat().toString()); // Prints the scan format (qrcode, pdf417 etc.)
+
+        // If you would like to resume scanning, call this method below:
+        mScannerView.resumeCameraPreview(this);
+    }
+}
