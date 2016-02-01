e67e29f55db6f7937f3558016d21c4ed74bbe92c
Nideesh Terapalli <Nid@nideeshs-mbp.dynamic.ucsd.edu>
2016-01-31 19:10:51 -0800

    Working QR and Response

--- SimpleScannerActivity/app/src/main/java/nid/simplescanneractivity/SimpleScannerActivity.java
+++ SimpleScannerActivity/app/src/main/java/nid/simplescanneractivity/SimpleScannerActivity.java
@@ -8,18 +8,24 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.content.Context;
+import android.widget.Toast;
 
 
 
 import me.dm7.barcodescanner.zxing.ZXingScannerView;
 import com.google.zxing.Result;
 import android.util.Log;
+import android.content.Intent;
 
 
 public class SimpleScannerActivity extends AppCompatActivity implements ZXingScannerView.ResultHandler{
 
     private ZXingScannerView mScannerView;
     private String TAG = "SimeScannerActivity";
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,6 +55,22 @@ public class SimpleScannerActivity extends AppCompatActivity implements ZXingSca
         // Do something with the result here
         Log.v(TAG, rawResult.getText()); // Prints scan results
         Log.v(TAG, rawResult.getBarcodeFormat().toString()); // Prints the scan format (qrcode, pdf417 etc.)
+
+        Intent intent = new Intent(this, poop.class);
+        if(rawResult.getText().equals("http://www.google.com"))
+        {startActivity(intent);}
+        else
+        {
+           Context context = getApplicationContext();
+            CharSequence text = "We da real mufukkas";
+            int duration = Toast.LENGTH_SHORT;
+
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+        }
+
+
+
 
         // If you would like to resume scanning, call this method below:
         mScannerView.resumeCameraPreview(this);
