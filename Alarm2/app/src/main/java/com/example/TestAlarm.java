5317b34c625c22b937e691a3dcb57774ec8effbf
MichaelMGonzalez <mmg005@ucsd.edu>
2016-02-08 11:55:59 -0800

    Added date picker and labels

--- Alarm2/app/src/main/java/com/example/TestAlarm.java
+++ Alarm2/app/src/main/java/com/example/TestAlarm.java
@@ -2,6 +2,7 @@ package com.example;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
 
@@ -10,6 +11,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.format.DateFormat;
 import android.view.View;
+import android.widget.DatePicker;
 import android.widget.TimePicker;
 
 import java.util.Calendar;
@@ -18,6 +20,7 @@ import nid.alarm.R;
 
 public class TestAlarm extends Activity {
 
+    // Main class methods
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,10 +29,21 @@ public class TestAlarm extends Activity {
 
     public void showTimePickerDialog(View v) {
         DialogFragment newFragment = new TimePickerFragment();
+        // The first parameter grabs the fragment manager and makes the transition possible
+        // No tutorial properly explained this so I don't really know the theory behind it
         newFragment.show(getFragmentManager().beginTransaction(), "timePicker");
     }
 
+    public void showDatePickerDialog(View v) {
+        DialogFragment newFragment = new DatePickerFragment();
+        // The first parameter grabs the fragment manager and makes the transition possible
+        // No tutorial properly explained this so I don't really know the theory behind it
+        newFragment.show(getFragmentManager().beginTransaction(), "datePicker");
+    }
 
+
+
+    // Time Picker Static Class
     public static class TimePickerFragment extends DialogFragment
             implements TimePickerDialog.OnTimeSetListener {
 
@@ -47,6 +61,28 @@ public class TestAlarm extends Activity {
 
         public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
             // Do something with the time chosen by the user
+        }
+    }
+
+
+    // Date Picker Static Class
+    public static class DatePickerFragment extends DialogFragment
+            implements DatePickerDialog.OnDateSetListener {
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the current date as the default date in the picker
+            final Calendar c = Calendar.getInstance();
+            int year = c.get(Calendar.YEAR);
+            int month = c.get(Calendar.MONTH);
+            int day = c.get(Calendar.DAY_OF_MONTH);
+
+            // Create a new instance of DatePickerDialog and return it
+            return new DatePickerDialog(getActivity(), this, year, month, day);
+        }
+
+        public void onDateSet(DatePicker view, int year, int month, int day) {
+            // Do something with the date chosen by the user
         }
     }
 
