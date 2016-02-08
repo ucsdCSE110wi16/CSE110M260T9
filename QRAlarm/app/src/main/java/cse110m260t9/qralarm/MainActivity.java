69e19fbc208a8504b934c2247c24f9d32b376029
itstehkman <itstehkman@gmail.com>
2016-02-07 17:15:53 -0800

    Add in alarm clock as starting activity. Bugs to fix:
    0) Trigger QRScannerActivity when the pendingIntent is run,
    and make sure to play the music at the same time too
    
    1) Make the alarm cancel turn off the music

--- QRAlarm/app/src/main/java/cse110m260t9/qralarm/MainActivity.java
+++ QRAlarm/app/src/main/java/cse110m260t9/qralarm/MainActivity.java
@@ -1,52 +1,88 @@
 package cse110m260t9.qralarm;
 
+import java.util.Calendar;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
-
-public class MainActivity extends AppCompatActivity {
-
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+public class MainActivity extends FragmentActivity{
+    private static int timeHour = Calendar.getInstance().get(Calendar.HOUR_OF_DAY);
+    private static int timeMinute = Calendar.getInstance().get(Calendar.MINUTE);
+    TextView timeText;
+    private static TextView alarmRingText;
+    public static TextView getAlarmRingText() {
+        return alarmRingText;
+    }
+    AlarmManager alarmManager;
+    private PendingIntent pendingIntent;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        timeText = (TextView)findViewById(R.id.msg1);
+        timeText.setText(timeHour + ":" + timeMinute);
+        alarmRingText = (TextView)findViewById(R.id.msg2);
 
-       FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
+        alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+        Intent myIntent = new Intent(MainActivity.this, AlarmReceiver.class);
+        pendingIntent = PendingIntent.getBroadcast(MainActivity.this, 0, myIntent, 0);
+
+        OnClickListener listener1 = new OnClickListener() {
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                alarmRingText.setText("");
+                Bundle bundle = new Bundle();
+                bundle.putInt(MyConstants.HOUR, timeHour);
+                bundle.putInt(MyConstants.MINUTE, timeMinute);
+                MyDialogFragment fragment = new MyDialogFragment(new SetAlarmHandler());
+                fragment.setArguments(bundle);
+                FragmentManager manager = getSupportFragmentManager();
+                FragmentTransaction transaction = manager.beginTransaction();
+                transaction.add(fragment, MyConstants.TIME_PICKER);
+                transaction.commit();
             }
-        });
+        };
+
+        Button start = (Button)findViewById(R.id.button1);
+        start.setOnClickListener(listener1);
+        OnClickListener listener2 = new OnClickListener() {
+            public void onClick(View view) {
+                alarmRingText.setText("");
+                cancelAlarm();
+            }
+        };
+        Button stop = (Button)findViewById(R.id.button2);
+        stop.setOnClickListener(listener2);
     }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    class SetAlarmHandler extends Handler {
+        @Override
+        public void handleMessage (Message msg){
+            Bundle bundle = msg.getData();
+            timeHour = bundle.getInt(MyConstants.HOUR);
+            timeMinute = bundle.getInt(MyConstants.MINUTE);
+            timeText.setText(timeHour + ":" + timeMinute);
+            setAlarm();
         }
+    }
+    private void setAlarm(){
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.HOUR_OF_DAY, timeHour);
+        calendar.set(Calendar.MINUTE, timeMinute);
+        alarmManager.setExact(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pendingIntent);
+    }
 
-        return super.onOptionsItemSelected(item);
+    private void cancelAlarm() {
+        if (alarmManager!= null) {
+            //TODO: make the music stop. currently the text goes away, but not the music
+            alarmManager.cancel(pendingIntent);
+        }
     }
 }
