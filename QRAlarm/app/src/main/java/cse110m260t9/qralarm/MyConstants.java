8fd0315733e92fd97c5251af1d31496e0bfbdca8
Carlos Chulo <thecompscientist.dev@gmail.com>
2016-02-21 23:25:47 -0800

    readded my constants for main activity/location

--- QRAlarm/app/src/main/java/cse110m260t9/qralarm/MyConstants.java
+++ QRAlarm/app/src/main/java/cse110m260t9/qralarm/MyConstants.java
@@ -9,5 +9,10 @@ public abstract class MyConstants {
     public static final String TIME_PICKER = "time_picker";
     public static final String LOCATION_FILE = "QRAlarm_Saved_Location_Info";
     public static final String QR_HASH_FILE = "QRAlarm_QR_Hash";
+    public final static String NEW_LOCATION = "New Location";
+    public final static String CURR_LOCATION = "Location";
+    public final static String LOCATION_NOT_SET = "Location not set";
+    public final static String LOCATION_SET = "Location set";
+    public final static int RESULT_CODE = 0x20;
 
 }
