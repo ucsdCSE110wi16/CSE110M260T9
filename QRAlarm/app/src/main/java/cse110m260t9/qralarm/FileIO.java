2e1072c745b0c5bd526e945e9465fcd799ddcc53
MichaelMGonzalez <mmg005@ucsd.edu>
2016-02-21 20:55:17 -0800

    Added more functions to FileIO with commenting

--- QRAlarm/app/src/main/java/cse110m260t9/qralarm/FileIO.java
+++ QRAlarm/app/src/main/java/cse110m260t9/qralarm/FileIO.java
@@ -2,6 +2,7 @@ package cse110m260t9.qralarm;
 
 import android.content.Context;
 
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -13,8 +14,12 @@ import java.io.IOException;
 public class FileIO {
     public static void saveString( String fileName, String stringToSave, Context ctx ) {
         try {
+            // Create the FileOutputStream
+            // The contex mode private ensures this file should only be used by our app
             FileOutputStream fileOutStream = ctx.openFileOutput(fileName, Context.MODE_PRIVATE);
+            // Dump the string to a file
             fileOutStream.write(stringToSave.getBytes());
+            // Close the stream
             fileOutStream.close();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
@@ -22,5 +27,27 @@ public class FileIO {
         catch (IOException e){
             e.printStackTrace();
         }
+    }
+
+    public static String retrieveStringFromFile( String fileName, Context ctx ) {
+        String retVal = "";
+        try {
+            // Create the FileInputStream
+            FileInputStream fileInStream = ctx.openFileInput(fileName);
+            // Grab a byte from the file stream and append it to the end of retVal
+            // -1 Signifies the EOF so we stop reading there
+            for( int i = fileInStream.read(); i != -1; i = fileInStream.read() ) {
+                retVal += (char) i;
+            }
+            // Close the stream
+            fileInStream.close();
+            // Error handling
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+        return retVal;
     }
 }
