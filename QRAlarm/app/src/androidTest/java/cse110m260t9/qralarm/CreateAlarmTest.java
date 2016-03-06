c1ae214edc309fa701c5ffe7df67a864fdd290ac
MichaelMGonzalez <mmg005@ucsd.edu>
2016-03-06 15:07:37 -0800

    Got espresso libraries to link properly

---
+++ QRAlarm/app/src/androidTest/java/cse110m260t9/qralarm/CreateAlarmTest.java
@@ -0,0 +1,47 @@
+package cse110m260t9.qralarm;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+
+import static android.support.test.espresso.Espresso.*;
+import static android.support.test.espresso.action.ViewActions.*;
+import static android.support.test.espresso.assertion.ViewAssertions.*;
+import static android.support.test.espresso.matcher.ViewMatchers.*;
+
+
+/**
+ * Created by Michael on 3/6/2016.
+ */
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class CreateAlarmTest {
+    private String mStringToBetyped;
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(
+            MainActivity.class);
+
+    @Before
+    public void initValidString() {
+        // Specify a valid string.
+        mStringToBetyped = "Espresso";
+    }
+
+    @Test
+    public void changeText_sameActivity() {
+        // Type text and then press the button.
+        /**onView(withId(R.id.editTextUserInput))
+                .perform(typeText(mStringToBetyped), closeSoftKeyboard());
+        onView(withId(R.id.changeTextBt)).perform(click());
+
+        // Check that the text was changed.
+        onView(withId(R.id.textToBeChanged))
+                .check(matches(withText(mStringToBetyped)));**/
+    }
+}
